47a7fd4d87cf9cafb267afac0bd1efc95f0ae33d
karimou cisse <karimou.cisse1@gmail.com>
2022-04-23 20:14:49 +0200

    create models folder and create user model

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const UserSchema = mongoose.Schema({
+    role: { type: String },
+    firstName: {type: String, required : true},
+    name: {type: String, required : true},
+    birthDate: {type: Date, required : true},
+    email: {type: String, required : true, unique: true},
+    password : { type : String, required : true, minlength: 6 },
+    phoneNumber : { type : String },
+    adress : { type : String, required : true},
+    orders: [{ type : Schema.Types.ObjectId, ref : "Order" }],
+    cart : { type : Schema.Types.ObjectId, ref : "Cart"},
+    paymentMethods : [{ type : Schema.Types.ObjectId, ref : "PaymentMethod"}],
+}, {
+    timestamps : true
+})
+
+module.exports = mongoose.model('User', UserSchema)
